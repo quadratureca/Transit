47ce9c8571ea6143fc73fb6cebf3edc0c2591202
Phil Garrett <phil.garrett@outlook.com>
2023-07-10 20:45:36 -0400

    Check in

--- TransitWorker/Position/Mississauga.cs
+++ TransitWorker/Position/Mississauga.cs
@@ -95,8 +95,6 @@ public class Mississauga : BackgroundService
                 try
                 {
                     string mississauga = "https://www.miapp.ca/GTFS_RT/Vehicle/VehiclePositions.pb";
-                    //string yrt = "https://rtu.yrt.ca/gtfsrealtime/VehiclePositions";
-                    //string goTransit = "http://api.openmetrolinx.com/OpenDataAPI/api/V1/Gtfs.proto/Feed/VehiclePosition?key=30023110";
                     //string barrie = "http://www.myridebarrie.ca/gtfs/GTFS_TripUpdates.pb";
                     //string hamilton = "https://opendata.hamilton.ca/GTFS-RT/GTFS_VehiclePositions.pb";
 
