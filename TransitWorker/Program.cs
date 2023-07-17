f3e6b530a6b404712af08339690606025178b084
Phil Garrett <phil.garrett@outlook.com>
2023-07-16 21:18:49 -0400

    Check in

--- TransitWorker/Program.cs
+++ TransitWorker/Program.cs
@@ -6,14 +6,14 @@ IHost host = Host.CreateDefaultBuilder(args)
     .UseSystemd()
     .ConfigureServices(services =>
     {
-        //services.AddHostedService<TransitWorker.Position.Mississauga>();
+        services.AddHostedService<TransitWorker.Position.Mississauga>();
         //services.AddHostedService<TransitWorker.Route.Mississauga>();
-        //services.AddHostedService<TransitWorker.Position.GoTransit>();
+        services.AddHostedService<TransitWorker.Position.GoTransit>();
         //services.AddHostedService<TransitWorker.Route.GoTransit>();
-        //services.AddHostedService<TransitWorker.Position.YRT>();
+        services.AddHostedService<TransitWorker.Position.YRT>();
         //services.AddHostedService<TransitWorker.Route.YRT>();
         services.AddHostedService<TransitWorker.Position.TTC>();
-        services.AddHostedService<TransitWorker.Route.TTC>();
+        //services.AddHostedService<TransitWorker.Route.TTC>();
     })
     .Build();
 
